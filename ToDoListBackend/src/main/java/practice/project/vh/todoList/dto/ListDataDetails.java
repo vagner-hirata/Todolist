abf2525633bfdfe36c5d091b034d98ac906e118d
Vagner Hirata <vagner.hirata@gmail.com>
2024-06-04 21:12:48 -0300

    Deleted unusable code

--- ToDoListBackend/src/main/java/practice/project/vh/todoList/dto/ListDataDetails.java
+++ ToDoListBackend/src/main/java/practice/project/vh/todoList/dto/ListDataDetails.java
@@ -2,8 +2,6 @@ package practice.project.vh.todoList.dto;
 
 import practice.project.vh.todoList.classes.ToDoList;
 
-import java.time.LocalDateTime;
-
 public record ListDataDetails(Long id, String description) {
 
     public ListDataDetails(ToDoList toDoList) {
